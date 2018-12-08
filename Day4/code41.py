482fbcdb83691fb387d1cbd188d2aa566e28fa76
CoenWVDO <vwehv@hotmail.com>
2018-12-08 10:54:23 +0100

    code 41 start

---
+++ Day4/code41.py
@@ -0,0 +1,15 @@
+#def sortFucntion:
+#  
+#  return int(?*) 
+f = open("input.txt", "r")
+
+inputList = []
+
+for line in f:
+    stringList.append(line.rstrip('\n'))
+
+f.close()
+
+inputlist.sort()
+
+print inputlist
